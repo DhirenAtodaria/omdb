2739c44d5b8c3071f2db516ea57ce1d0b0b6f6c6
DhirenAtodaria <dhiren.atodaria@hotmail.co.uk>
2020-04-01 19:56:59 +0100

    Updates

---
+++ precache-manifest.0b3ef7de2a4397709a27c9c677c25129.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cfcf08e066ea0db8dd1e224ba14db0e",
+    "url": "/omdb/index.html"
+  },
+  {
+    "revision": "7574c2593a5f5405ae8e",
+    "url": "/omdb/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "9365e115a1c29f1086ca",
+    "url": "/omdb/static/css/main.22623491.chunk.css"
+  },
+  {
+    "revision": "7574c2593a5f5405ae8e",
+    "url": "/omdb/static/js/2.cd9f801b.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/omdb/static/js/2.cd9f801b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9365e115a1c29f1086ca",
+    "url": "/omdb/static/js/main.dc63c5df.chunk.js"
+  },
+  {
+    "revision": "570f23336e67ba78bbc0",
+    "url": "/omdb/static/js/runtime-main.2e866f92.js"
+  }
+]);
